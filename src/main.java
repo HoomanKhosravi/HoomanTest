9faa4df4ffc7c947b57b759a73cb249ff8b9d952
Hooman Khosravi <>
2018-03-06 02:38:55 -0700

    Changing text

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 
 public class main {
 	public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello Everyone"); // Display the string.
         
     }
 }
